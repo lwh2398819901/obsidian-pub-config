137be306463bbac7c857f7155979d2e257c81e24
liuwh <liuwenhao@uniontech.com>
2022-09-23 15:38:40 +0800

    更新结构

---
+++ syncLocal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ./.obsidian
+cp -r ../.obsidian ./
+
+rm -rf "./.00 关于本资料库的使用方式"
+cp -r "../00 关于本资料库的使用方式" ".00 关于本资料库的使用方式"
+
+rm -rf "./.00-1 obsidian 模板"
+cp -r "../00-1 obsidian 模板" ".00-1 obsidian 模板"
